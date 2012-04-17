6e74a4a0b72541e6ef516e6039bda94ac61a6430
Robert E <robert-edelmann@web.de>
2012-04-17 17:20:38 +0200

    erste Version der GUI

---
+++ Hash/Ed2k.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AniSharp.Hash
+{
+    /// <summary>
+    /// Made by dmitry.baykov@gmail.com
+    /// </summary>
+    class Ed2k:IHash
+    {
+        MD4 md4, root;
+        int lastLength;
+        const int chunkSize = 9728000;
+        bool smallFile = true;
+
+        public Ed2k() { md4 = new MD4(); root = new MD4(); Reset(); }
+
+        public void Reset()
+        {
+            md4.Reset();
+            root.Reset();
+            lastLength = 0;
+            smallFile = true;
+        }
+
+        public void Append(byte value)
+        {
+            lock (this)
+            {
+                md4.Append(value);
+                lastLength++;
+                if (lastLength == chunkSize)
+                {
+                    smallFile = false;
+                    root.Append(md4.Finish());
+                    md4.Reset();
+                    lastLength = 0;
+                }
+            }
+        }
+
+        public void Append(byte[] buffer)
+        {
+            if (buffer == null)
+                throw new ArgumentNullException();
+
+            Append(buffer, 0, buffer.Length);
+        }
+        byte[] cache = new byte[chunkSize];
+
+        public void Append(byte[] buffer, int offset, int count)
+        {
+            if (buffer == null) throw new ArgumentNullException("buffer");
+            if (count < 0) throw new ArgumentOutOfRangeException("count", "Count cannot be less than zero");
+            if (offset < 0 || offset + count > buffer.Length) throw new ArgumentOutOfRangeException("offset");
+
+            lock (this)
+            {
+                while (count >= chunkSize - lastLength)
+                {
+                    Array.Copy(buffer, offset, cache, 0, chunkSize - lastLength);
+                    md4.Append(cache, 0, chunkSize - lastLength);
+                    root.Append(md4.Finish());
+                    md4.Reset();
+                    count -= chunkSize - lastLength;
+                    offset += chunkSize - lastLength;
+                    lastLength = 0;
+                    smallFile = false;
+                }
+                if (count > 0)
+                {
+                    md4.Append(buffer, offset, count);
+                    lastLength += count;
+                }
+            }
+        }
+
+        public byte[] Finish()
+        {
+            if (smallFile)
+                return md4.Finish();
+            root.Append(md4.Finish());
+
+            return root.Finish();
+        }
+    }
+}
---
+++ Hash/HashGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AniSharp.Hash
+{
+    enum HashType
+    {
+        Ed2k,MD4,CRC32,MD5,SHA1
+    }
+    /// <summary>
+    /// HashGenerator
+    /// </summary>
+    class HashGenerator
+    {
+        String _sFile;
+        System.IO.FileStream _FileStream;
+        System.IO.BinaryReader _BinaryReader;
+        Int32 _length;
+        IHash _hash;
+        HashType _htType;
+        public HashGenerator(String sFile)
+        {
+            _sFile = sFile;
+            _FileStream = new System.IO.FileStream(sFile, System.IO.FileMode.Open, System.IO.FileAccess.Read);
+            _BinaryReader = new System.IO.BinaryReader(_FileStream);
+            _length = (Int32)(new System.IO.FileInfo(sFile).Length);
+        }
+
+        public void Generate(HashType htType)
+        {
+            _htType = htType;
+            switch (htType)
+            {
+                case HashType.Ed2k: _hash = new Ed2k(); break;
+                case HashType.MD4: _hash = new MD4(); break;
+                case HashType.CRC32: _hash = new Crc32();break ;
+                case HashType.MD5: _hash = new MD5(); break;
+                case HashType.SHA1: _hash = new Sha1(); break;
+                default: throw new NotImplementedException();
+            }
+            _hash.Reset();
+            try
+            {
+                _hash.Append(_BinaryReader.ReadBytes(_length));
+            }
+            catch (Exception) { }
+            _FileStream.Close();
+            _FileStream.Dispose();
+            _BinaryReader.Close();
+        }
+        
+        override public String ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            String sReturn = "";
+            byte[] hash = _hash.Finish();
+            for (int i = 0; i < hash.Length; i++)
+                sb.AppendFormat("{0:x2}", hash[i]);
+            switch (_htType)
+            {
+                case HashType.Ed2k: sReturn = "ed2k://|file|" + _sFile.Substring(_sFile.LastIndexOf(@"\")).Replace(@"\", "") + "|" + _length + "|"+sb.ToString()+"|"; break;
+                default: break;
+            }
+            return sReturn;
+        }
+    }
+}
