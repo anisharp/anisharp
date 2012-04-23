3bb4b4350028c854558bbac160967d889a22a755
b00hm4nn <b00hm4nn@googlemail.com>
2012-04-23 20:03:16 +0200

    Connection test added

---
+++ DatabaseConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data.SqlServerCe;
+using System.Data;
+
+namespace AniSharp
+{
+    class DatabaseConnection
+    {
+        public void testConnectivity()
+        {
+            MainWindow mw = new MainWindow();
+            try {
+                SqlCeConnection con = new SqlCeConnection(@"Data Source=AniSharpDB.sdf; Persist Security Info=False;");
+                con.Open();  
+                mw.lbDatabase_Add("OK");
+                MessageBox.Show("OK");
+                con.Close();             
+            }
+            catch (Exception) {
+                mw.lbDatabase_Add("Failed !");
+                MessageBox.Show("Failed !");
+            }
+        }
+    }
+}
