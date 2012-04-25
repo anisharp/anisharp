dae08b873ab7140de3336c40ef2f19e0a320f15c
Jonas Weber <mail@jonasw.de>
2012-04-25 08:25:33 +0200

    defined ApiAnswer

---
+++ API/ApiAnswer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AniSharp.API
+{
+
+    /// <summary>
+    /// Encapsulats an API-Answer
+    /// </summary>
+    abstract class ApiAnswer
+    {
+	
+	private ReturnCode code;
+	public ReturnCode Code {
+		get
+		{
+			return code;
+		}
+	}
+
+
+	public String GetMessage()
+	{
+		return code.ToString("F");
+	}
+
+
+	public ApiAnswer(int code)
+	{
+		try {
+			this.code = (ReturnCode) code;
+		} catch (InvalidCastException) {
+			throw new ArgumentException();
+		}
+	}
+    }
+}
