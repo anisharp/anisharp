8619e3c49b4d103495ea3ea15f3bbfb1a14a71a7
Jonas Weber <mail@jonasw.de>
2012-05-08 14:53:47 +0200

    apidapter sets encoding, database re-introduced ;-)

--- API/ApiAdapter.cs
+++ API/ApiAdapter.cs
@@ -12,9 +12,9 @@ using AniSharp.API.Model;
 
 namespace AniSharp.API
 {
-    /// <summary>
-    /// Sends commands to AniDB, and receives their counterparts
-    /// </summary>
+	/// <summary>
+	/// Sends commands to AniDB, and receives their counterparts
+	/// </summary>
 	class ApiAdapter : Queryable
 	{
 		private BlockingCollection<String> outQueue = new BlockingCollection<String>();
@@ -22,14 +22,16 @@ namespace AniSharp.API
 		private Thread senderThread;
 		private Thread receiverThread;
 
-        private UdpAdapter udpadapter;
+		private UdpAdapter udpadapter;
 
 		public const int WAITING_BETWEEN_PACKETS = 2000;
-        public const string TAG_NOT_GIVEN_TAG = String.Empty;
+		public const string TAG_NOT_GIVEN_TAG = "";
+
+
 
 		public ApiAdapter()
 		{
-            System.Net.IPHostEntry host = System.Net.Dns.GetHostEntry(AniSharp.Properties.Settings.Default.Address);
+			System.Net.IPHostEntry host = System.Net.Dns.GetHostEntry(AniSharp.Properties.Settings.Default.Address);
 			udpadapter = new DefaultUdpAdapter(new IPEndPoint(host.AddressList[0], AniSharp.Properties.Settings.Default.RemotePort));
 
 			senderThread = new Thread(senderThreadInit);
@@ -43,7 +45,7 @@ namespace AniSharp.API
 			senderThread.Interrupt();
 			receiverThread.Interrupt();
 
-            udpadapter.shutdown();
+			udpadapter.shutdown();
 		}
 
 
@@ -54,7 +56,13 @@ namespace AniSharp.API
 				while (true)
 				{
 					String toSend = outQueue.Take();
-                    udpadapter.send(toSend);
+                    lock (toSend)
+                    {
+                        System.Diagnostics.Debug.Print("got lock for command, sending now");
+                        udpadapter.send(toSend);
+
+                        Monitor.PulseAll(toSend);
+                    }
 
 					Thread.Sleep(WAITING_BETWEEN_PACKETS);
 				}
@@ -75,15 +83,34 @@ namespace AniSharp.API
 			String tag = generateTag();
 			req["tag"] = tag;
 
-			outQueue.Add(req.ToString());
+			if (req.Command.Equals("AUTH"))
+			{
+				req["enc"] = AniSharp.Properties.Settings.Default.TransportEncoding;
+                System.Diagnostics.Debug.Print("This is a login, so appending enc");
+			}
+            String outString = req.ToString();
+            lock (outString)
+            {
+                outQueue.Add(outString);
+
+                if (req.Command.Equals("AUTH"))
+                {
+                    System.Diagnostics.Debug.Print("waiting for outString to be pulsed");
+                    Monitor.Wait(outString);
+                    System.Diagnostics.Debug.Print("outString pulsed, setting encoding");
+                    udpadapter.TransportEncoding = Encoding.GetEncoding(AniSharp.Properties.Settings.Default.TransportEncoding);
+                }
+            }
+            System.Diagnostics.Debug.Print("command enqueued, waiting for result");
 			while (!results.ContainsKey(tag))
 				lock (results)
 				{
 					Monitor.Wait(results);
 				}
+            System.Diagnostics.Debug.Print("and we have result");
 			String result = results[tag];
 			// drop answer from dictionary?
-            return ApiAnswer.parse(result);
+			return ApiAnswer.parse(result);
 		}
 
 		//TODO: uniquify TAGS!
@@ -96,7 +123,7 @@ namespace AniSharp.API
 			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < TAG_LEN; i++)
 			{
-                
+
 				sb.Append(TAG_CHARS[RND.Next(TAG_CHARS.Length)]);
 			}
 			return sb.ToString();
@@ -108,28 +135,28 @@ namespace AniSharp.API
 			{
 				while (true)
 				{
-                    String returnData = udpadapter.receive();
+					String returnData = udpadapter.receive();
 
-                    int index = returnData.IndexOf(' ');
-                    if (index < 0)
-                    {
-                        // we received garbage
-                        System.Diagnostics.Debug.Print("received garbage from AniDB: " + returnData + '\n');
-                        continue;
-                    }
+					int index = returnData.IndexOf(' ');
+					if (index < 0)
+					{
+						// we received garbage
+						System.Diagnostics.Debug.Print("received garbage from AniDB: " + returnData + '\n');
+						continue;
+					}
 
-                    String firstElem = returnData.Substring(0, index);
-                    if (firstElem.Length == 3 && Int32.Parse(firstElem) > 0)
-                    {
-                        // not tagged
-                        results.Add(TAG_NOT_GIVEN_TAG, returnData);
-                    }
-                    else
-                    {
-                        // we have tag
-                        String strippedData = returnData.Substring(index + 1);
-                        results.Add(firstElem, strippedData);
-                    }
+					String firstElem = returnData.Substring(0, index);
+					if (firstElem.Length == 3 && Int32.Parse(firstElem) > 0)
+					{
+						// not tagged
+						results.Add(TAG_NOT_GIVEN_TAG, returnData);
+					}
+					else
+					{
+						// we have tag
+						String strippedData = returnData.Substring(index + 1);
+						results.Add(firstElem, strippedData);
+					}
 
 					lock (results)
 					{
